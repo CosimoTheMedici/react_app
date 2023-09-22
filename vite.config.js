ea8e43f58f4b53da651fb934064339f7a3953a55
cosmas <cosmasthuku4@gmail.com>
2023-09-22 16:37:51 +0300

    fix:vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
+// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   optimizeDeps: {
-//     // include: ['linked-dep'],
-//   },
-//   build: {
-//     commonjsOptions: {
-//       transformMixedEsModules: true,
-//       // include: [/linked-dep/, /node_modules/],
-//     },
-//   },
-// });
+// })
+
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    // include: ['linked-dep'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
+});
